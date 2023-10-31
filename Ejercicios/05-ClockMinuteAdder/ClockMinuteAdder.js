97bf4bddbb97c18530bad8fee2c8835ad4c14b06
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-10-31 19:28:20 -0300

    clock Minute

--- Ejercicios/05-ClockMinuteAdder/ClockMinuteAdder.js
+++ Ejercicios/05-ClockMinuteAdder/ClockMinuteAdder.js
@@ -1,6 +1,26 @@
 function clockMinuteAdder (time, minutesToAdd) {
   // Your code here:
+  const [hourString, minuteString] = time.split(':')
+  const hours = parseInt(hourString)
+  const minutes = parseInt(minuteString)
+
+  const hoursToMinutes = hours * 60
+  const totalMinutes = hoursToMinutes + minutes + minutesToAdd
+  const newMinute = totalMinutes % 60
+  let newHour = Math.floor(totalMinutes / 60) % 12
+  if(newHour === 0) newHour = 12
+
+  //console.log(newHour);
+
+
+  return `${newHour < 10 ? '0' + newHour : newHour}:${newMinute < 10 ? '0' + newMinute : newMinute}`;
+
+
+   
+  //console.log(newHours);
 
 }
+
+console.log(clockMinuteAdder('12:05', 1440));
 
 module.exports = clockMinuteAdder
