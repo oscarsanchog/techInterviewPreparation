c79f3be6e55378ae3294d68738335f581de96927
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-11-01 23:31:08 -0300

    intersection

--- Ejercicios/06-Intersection/Intersection.js
+++ Ejercicios/06-Intersection/Intersection.js
@@ -1,6 +1,14 @@
 function intersection (arr1, arr2) {
   // Your code here:
+  const repeated = [];
 
+  return arr1.reduce((acc, curr, i) => {
+    if (arr2[i] === curr) acc.push(curr); // el elemento de arr2 en la posicion que está recorriendo actualemtne el reducer es igual al elemento del arr1, lo pusheaa repeated. Sabe que acc es repeated porque se lo indiqué como tercer parámetro
+    return acc 
+  }, repeated);
 }
+
+
+console.log(intersection([1, 3, 5, 7, 10], [2, 3, 6, 8, 10, 20]));
 
 module.exports = intersection
